4142f8761b4d9997d9aaaa3cc06b6550f6658569
anasanwar2 <57671285+anasanwar2@users.noreply.github.com>
2019-11-13 16:28:08 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,13 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", () => {
+let channel =     client.channels.get("643813432772132867")
+setInterval(function() {
+channel.send(`heo wew we go in the stret and apelwse and with your mother and your dog els morninig go to the bitch senema and goinig and he ghmere ywer jknkenwr khn anas anas hrew and wew yuo gone they heo wew we go in the stret and apelwse and with your mot heo wew we go in the stret and apelwse and with your mot heo wew we go in the stret and apelwse and with your mot heo wew we go in the stret and apelwse and with your mot heo wew we go in the stret and apelwse and with your mot `);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
